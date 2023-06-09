52eb031ad28356fbc448c009a408480aca4fd99f
Felix <fkonprecht@gmail.com>
2023-06-09 16:21:18 +0200

    added new iteration of the ClientTicket

---
+++ Source/StoryObjectExample/StoryObject/StoryObjectClientPhaseTicket.cpp
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StoryObjectClientPhaseTicket.h"
+
+#include "StoryObjectExample/StoryObject/StoryObject.h"
+
+
+UStoryObjectClientPhaseTicket::UStoryObjectClientPhaseTicket()
+	: m_client(nullptr)
+	, m_endPhase(EStoryObjectPhase::IDLE)
+{ }
+
+UActorComponent* UStoryObjectClientPhaseTicket::GetClient() const
+{
+	return m_client;
+}
+
+EStoryObjectPhase UStoryObjectClientPhaseTicket::GetEndPhase() const
+{
+	return m_endPhase;
+}
+
+TArray<UActorComponent*> UStoryObjectClientPhaseTicket::GetDependencies() const
+{
+	return m_dependencies;
+}
+
+FClientFinishedPhase UStoryObjectClientPhaseTicket::RegisterClient(UActorComponent* client, const EStoryObjectPhase endPhase,
+																   const TArray<UActorComponent*> dependencies)
+{
+	m_client = client;
+	m_endPhase = endPhase;
+	m_dependencies = dependencies;
+
+	FClientFinishedPhase onClientFinishedPhaseEvent;
+	onClientFinishedPhaseEvent.BindDynamic(this, &UStoryObjectClientPhaseTicket::OnClientFinishedPhase);
+
+	return onClientFinishedPhaseEvent;
+}
+
+void UStoryObjectClientPhaseTicket::FulfillDependency(UActorComponent* dependency)
+{
+	m_dependencies.Remove(dependency);
+
+	if (m_dependencies.Num() <= 0 && OnClientDependenciesFulfilledEvent.IsBound())
+		OnClientDependenciesFulfilledEvent.Execute(this);
+}
+
+void UStoryObjectClientPhaseTicket::OnClientFinishedPhase()
+{
+	if (OnClientFinishedPhaseEvent.IsBound())
+		OnClientFinishedPhaseEvent.Execute(this);
+}
---
+++ Source/StoryObjectExample/StoryObject/StoryObjectClientPhaseTicket.h
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "StoryObjectClientPhaseTicket.generated.h"
+
+class UStoryObjectClientPhaseTicket;
+
+
+DECLARE_DYNAMIC_DELEGATE(FClientFinishedPhase);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FTicketStateChanged, UStoryObjectClientPhaseTicket*, ticket);
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class STORYOBJECTEXAMPLE_API UStoryObjectClientPhaseTicket : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite)
+	FTicketStateChanged OnClientDependenciesFulfilledEvent;
+
+	UPROPERTY(BlueprintReadWrite)
+	FTicketStateChanged OnClientFinishedPhaseEvent;
+	
+protected:
+	UPROPERTY()
+	UActorComponent* m_client;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	EStoryObjectPhase m_endPhase;
+
+	UPROPERTY()
+	TArray<UActorComponent*> m_dependencies;
+
+public:
+	UStoryObjectClientPhaseTicket();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	UActorComponent* GetClient() const;
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	EStoryObjectPhase GetEndPhase() const;
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	TArray<UActorComponent*> GetDependencies() const;
+
+	UFUNCTION(BlueprintCallable)
+	FClientFinishedPhase RegisterClient(UActorComponent* client, EStoryObjectPhase endPhase, TArray<UActorComponent*> dependencies);
+	
+	UFUNCTION(BlueprintCallable)
+	void FulfillDependency(UActorComponent* dependency);
+
+protected:
+	UFUNCTION()
+	void OnClientFinishedPhase();
+};
+
